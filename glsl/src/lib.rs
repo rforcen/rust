2ebcac9ac7140c15f7646da99fea10f3caac6008
roberto forcen <robertoforcen@gmail.com>
2021-03-24 13:27:52 +0100

    Delete lib.rs

--- glsl/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// lib.rs
-
-use std::io::Write;                                                                                                                                                                  
-use std::fs::File;   
-
-pub fn write_bin(file_name : &str, image : &[u8]) {
-	File::create(file_name).expect("Unable to create file")
-	.write_all(image).expect("Unable to write data"); // showbinimage.py w w mandelbrot.bin     
-}
